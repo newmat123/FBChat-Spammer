812c88e287764be79a0a93c9d40e678d75aff8f6
newmat123 <37107989+newmat123@users.noreply.github.com>
2020-01-13 16:59:48 +0100

    finished
    fun little thing

--- bot.py
+++ bot.py
@@ -1,20 +1,18 @@
 from fbchat import Client
 from fbchat.models import *
-
-
-#defins what messeges to send
-def definMessege():
-    global text
-
-    text = Message(input())
-    sendIt()
-
+import time
 
 #finds and opens my profile
 def findMe():
     global client
+    print("your phone number:")
+    Number = input("")
+    print("your password for facebook:")
+    Password = input("")
 
-    client = Client(xxxxxx, "xxxxxx")
+    print("lokking for user...")
+    client = Client(int(Number), Password)
+
     print("Own id: {}".format(client.uid))
     findUser()
 
@@ -23,25 +21,48 @@ def findMe():
 def findUser():
     global user
 
-    users = client.searchForUsers('nicklas')
+    print("who do you want to target?")
+    target = input("")
+
+    print("looking for target...")
+    users = client.searchForUsers(target)
     user = users[0]
 
     print("User's ID: {}".format(user.uid))
     print("User's name: {}".format(user.name))
     print("User's profile picture URL: {}".format(user.photo))
     print("User's main URL: {}".format(user.url))
+
     definMessege()
+
+
+#defins what messeges to send
+def definMessege():
+    global text
+
+    print("type the meseges you want to spam:")
+    text = Message(input())
+    sendIt()
 
 
 #sends it
 def sendIt():
-    client.send(text, thread_id=user.uid, thread_type=ThreadType.USER)
-    logoutBot()
+    print("how meany times do you want to send it?:")
+    x = input("")
+    i = 0
 
+    print("spaming...")
+    while i < int(x):
+        client.send(text, thread_id=user.uid, thread_type=ThreadType.USER)
+        i += 1
+        time.sleep(0.1)
 
-#logs out
-def logoutBot():
-    client.logout()
+    print("done")
+    print("")
+    print("")
+    print("")
+    findUser()
+
 
 
 #start
